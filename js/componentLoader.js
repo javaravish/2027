6284495e53a1ce700d08a92011643f4d5e1306ff
Ravi Kalyan <kollojuravikumar1@gmail.com>
2025-08-22 19:11:41 +0530

    asda

--- js/componentLoader.js
+++ js/componentLoader.js
@@ -45,8 +45,9 @@ const components = {
   header: `
     <!-- Head Section -->
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="author" content="RaviKalyan">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="js/images/tab_logo.png">
 
     <!-- Google Fonts -->
